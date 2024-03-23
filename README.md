176c9ba043ae2bd41a55e2bad48794411566bb7f
tacosontitan <contact@tacosontitan.com>
2024-03-23 13:55:19 -0500

    Standardized repository.

---
+++ README.md
@@ -0,0 +1,17 @@
+# 🌊 shoreline
+
+A simple chatbot for Pineapple Cove.
+
+![License](https://img.shields.io/github/license/tacosontitan/shoreline?logo=github&style=for-the-badge)
+![.NET Workflow](https://img.shields.io/github/actions/workflow/status/tacosontitan/shoreline/build.yml?label=Build%20and%20Test&logo=dotnet&style=for-the-badge)
+
+## 💁‍♀️ Getting Started
+
+Get started by reviewing the answers to the following questions:
+
+- [How do I navigate the codebase with confidence?](http://shoreline.tacosontitan.com)
+- [How do I report security concerns?](./SECURITY.md)
+- [How should I behave here?](./CODE_OF_CONDUCT.md)
+- [How can I help?](./CONTRIBUTING.md)
+- [What's the latest?](./resources/RELEASE_NOTES.md)
+- [Where are we going next?](./resources/ROADMAP.md)
--- src/Shoreline/Worker.cs
+++ src/Shoreline/Worker.cs
@@ -1,24 +1,14 @@
 namespace Shoreline;
 
-public class Worker : BackgroundService
+public class Worker(ILogger<Worker> logger)
+    : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
-
-    public Worker(ILogger<Worker> logger)
-    {
-        _logger = logger;
-    }
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (_logger.IsEnabled(LogLevel.Information))
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
-
-            await Task.Delay(1000, stoppingToken);
+            logger.LogInformation("Worker running at: {Timestamp}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken).ConfigureAwait(false);
         }
     }
 }
