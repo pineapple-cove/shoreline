ca90503cb47de12258f92a3a2c4b1311d7fba03d
tacosontitan <contact@tacosontitan.com>
2024-03-25 19:12:49 -0500

    Added commands project.

---
+++ src/Shoreline.Commands/Statistics/Uptime.cs
@@ -0,0 +1,16 @@
+﻿using Shoreline.Metrics;
+using Shoreline.Reflection;
+
+namespace Shoreline.Commands.Statistics;
+
+/// <summary>
+/// Represents the uptime of the hosting bot.
+/// </summary>
+[GlobalCommand("uptime", "Displays the uptime of the bot.")]
+public sealed class Uptime(IUptime uptime)
+    : IQuery<TimeSpan>
+{
+    /// <inheritdoc />
+    public Task<TimeSpan> Execute(CancellationToken cancellationToken = default) =>
+        Task.FromResult(uptime.Value);
+}
